4a1d94017d5029ecb6df42f66d3e84c455b5d4d9
Irvin Garcia <irvingarcia57@gmail.com>
2020-02-12 17:47:11 -0600

    Work in Progress

---
+++ app/src/main/java/com/example/theperfectpartner/AddBirthday.java
@@ -0,0 +1,23 @@
+package com.example.theperfectpartner;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AddBirthday extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.add_birthday, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/theperfectpartner/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.theperfectpartner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.add_birthday);
+    }
+}
