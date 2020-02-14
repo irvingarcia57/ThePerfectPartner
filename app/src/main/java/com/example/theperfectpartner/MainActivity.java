15b8a6079a45da9f8b5713a1a0c6d78337053bc6
IrvinG <irvingarcia57@gmail.com>
2020-02-14 17:59:23 -0600

    Added new fragments and navigation bar but haven't implemented

--- app/src/main/java/com/example/theperfectpartner/MainActivity.java
+++ app/src/main/java/com/example/theperfectpartner/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.add_birthday);
+        setContentView(R.layout.bottom_navigation);
     }
 }
