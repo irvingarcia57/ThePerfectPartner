dae4e7ca2e96c548bb9088825f1b9c5628dc6d0d
IrvinG <irvingarcia57@gmail.com>
2020-02-14 21:22:49 -0600

    Added new fragments and navigation bar but haven't implemented

--- app/src/main/java/com/example/theperfectpartner/MainActivity.java
+++ app/src/main/java/com/example/theperfectpartner/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.theperfectpartner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.bottom_navigation);
+        setContentView(R.layout.activity_main);
+
+//        BottomNavigationView bottomNav  = findViewById(R.id.bottom_navigation);
+//        bottomNav.OnNavigationItemReselectedListener(navListener);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                new HomeTab()).commit();
     }
+
+
+
 }
