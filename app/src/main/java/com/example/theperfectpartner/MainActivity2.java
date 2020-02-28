0fff09d02327c76627f81b967d7eca6245bb8627
IrvinG <irvingarcia57@gmail.com>
2020-02-27 21:33:33 -0600

    Created drawer Nav. Just working on clickables.

--- app/src/main/java/com/example/theperfectpartner/MainActivity2.java
+++ app/src/main/java/com/example/theperfectpartner/MainActivity2.java
@@ -1,34 +1,52 @@
 package com.example.theperfectpartner;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+//import androidx.fragment.app.Fragment;
+//import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+//import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
 
-
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView drawer_navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        //Bottom navigation logic
         BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
         NavController navController = Navigation.findNavController(this,R.id.fragment_container);
-
         NavigationUI.setupWithNavController(navigationView , navController);
+
+        //Drawer navigation logic
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolbar);
+        drawer_navigationView = findViewById(R.id.navigation_view);
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
 
 
@@ -39,5 +57,23 @@ public class MainActivity2 extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.drawer1:
+                Toast.makeText(this, "drawer 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.drawer2:
+                Toast.makeText(this, "drawer 2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.drawer3:
+                Toast.makeText(this, "drawer 3", Toast.LENGTH_SHORT).show();
+                break;
 
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
