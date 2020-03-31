a318aac8f64ecb985cd95564b6f2968ab81cba34
Irvin Garcia <irvingarcia57@gmail.com>
2020-03-30 21:06:03 -0500

    Designed and added the checkboxes but now I just have to set the onClick method for them. Once that is done, we can create 5 more different fragments using the same method.

--- app/src/main/java/com/example/theperfectpartner/AccountCreationFood.java
+++ app/src/main/java/com/example/theperfectpartner/AccountCreationFood.java
@@ -7,23 +7,92 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
-import com.example.theperfectpartner.R;
+import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class AccountCreationFood extends Fragment {
 
-    public AccountCreationFood() {
-        // Required empty public constructor
-    }
-
+public class AccountCreationFood extends Fragment implements View.OnClickListener {
+    private ArrayList<String> selection =new ArrayList<String>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.account_creation_food, container, false);
+
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+            boolean checked = ((CheckBox) view).isChecked();
+
+            if (view.getId() == R.id.burgers) {
+                if (checked) {
+                    selection.add("Burgers");
+                } else {
+                    selection.remove("Burgers");
+                }
+            }
+            if (view.getId() == R.id.italian) {
+                if (checked) {
+                    selection.add("Italian");
+                } else {
+                    selection.remove("Italian");
+                }
+            }
+            if (view.getId() == R.id.asian) {
+                if (checked) {
+                    selection.add("Asian");
+                } else {
+                    selection.remove("Asian");
+                }
+            }
+            if (view.getId() == R.id.mexican) {
+                if (checked) {
+                    selection.add("Mexican");
+                } else {
+                    selection.remove("Mexican");
+                }
+            }
+            if (view.getId() == R.id.tacos) {
+                if (checked) {
+                    selection.add("Tacos");
+                } else {
+                    selection.remove("Tacos");
+                }
+            }
+            if (view.getId() == R.id.indian) {
+                if (checked) {
+                    selection.add("Indian");
+                } else {
+                    selection.remove("Indian");
+                }
+            }
+            if (view.getId() == R.id.cajun) {
+                if (checked) {
+                    selection.add("Cajun");
+                } else {
+                    selection.remove("Cajun");
+                }
+            }
+            if (view.getId() == R.id.mediter) {
+                if (checked) {
+                    selection.add("Mediterranean");
+                } else {
+                    selection.remove("Mediterranean");
+                }
+            }
+            if (view.getId() == R.id.german) {
+                if (checked) {
+                    selection.add("German");
+                } else {
+                    selection.remove("German");
+                }
+            }
+
     }
 }
--- app/src/main/java/com/example/theperfectpartner/ActivityAccountCreation.java
+++ app/src/main/java/com/example/theperfectpartner/ActivityAccountCreation.java
@@ -1,14 +1,21 @@
 package com.example.theperfectpartner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+
 
 public class ActivityAccountCreation extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account_creation);
+        setContentView(R.layout.account_creation_food);
+
     }
+
+
 }
